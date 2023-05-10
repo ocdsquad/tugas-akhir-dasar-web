fafaa24a7b887fa30fc60390f7e83ca93ba1ba44
ahmad alfarel <84859336+ocdsquad@users.noreply.github.com>
2023-05-10 21:19:23 +0700

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,6 @@
+//untuk meredirect box ke website silidi
+let biodata = document.querySelector('.box');
+
+biodata.addEventListener('click', function(){
+    window.open('http://silidi.epizy.com/');
+});
